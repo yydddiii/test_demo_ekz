1d1bfc25073351ca33a5369a046f266c94dcf4c3
Леонид <154961348+yydddiii@users.noreply.github.com>
2024-11-11 13:10:22 +0300

    init

---
+++ demo_ekz/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using demo_ekz.DataApp;
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace demo_ekz
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    /// 
+
+    public partial class MainWindow : Window
+    {
+        public Products products = new Products();
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = products;
+            ProductGrid.ItemsSource = demo_yag_tolEntities.GetContext().Products.ToList();
+            
+        }
+    }
+}
